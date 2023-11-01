697a458556c393afb5e6f4f62d973bcc1bb4f0cc
Dacre <daniele.dominici18@gmail.com>
2023-11-01 11:37:29 +0100

    fixes gameID

--- dependencies/gameVersionChecker.lua
+++ dependencies/gameVersionChecker.lua
@@ -6,17 +6,28 @@ local tableGameCodes = {
     ["AGB-AXVE"] = {["gameID"] = "AXVE", ["name"] = "Ruby"},
     ["AGB-AXPE"] = {["gameID"] = "AXPE", ["name"] = "Sapphire"}
 }
+local gameVersions = {
+    ["AGB-BPRE"] = {[26624] = "BPR1", [26369] = "BPR2"}
+    ["AGB-ZBDM"] = {[33024] = "BPG1", [32769] = "BPG2"}
+}
 
 local mod = {}
 
 function mod.GetGameVersion()
-    GameCode = emu:getGameCode()
-	if not (tableGameCodes[GameCode]) then
+    local gameCode = emu:getGameCode()
+	if not (tableGameCodes[gameCode]) then
 		ConsoleForText:print("Unknown game. Script disabled.\n\n")
 		return false
-	end
-    ConsoleForText:print(tableGameCodes[GameCode]["name"] .. " detected. Script Enabled.\n\n")
-	return true, tableGameCodes[GameCode]["gameID"]
+    end
+    if(gameVersions[gameCode])
+        local gameVersion = emu:read16(134217916)
+        if gameVersions[gameCode][gameVersion]
+            tableGameCodes[gameCode].gameID = gameVersions[gameCode][gameVersion]
+        end
+    end
+
+    ConsoleForText:print(tableGameCodes[gameCode]["name"] .. " detected. Script Enabled.\n\n")
+	return true, tableGameCodes[gameCode].gameID
 end
 
 return mod
